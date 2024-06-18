ffc9dd8364b1ebb658d18fafd3e31eb05756f1dd
Bruce W. Herr II <bherr@indiana.edu>
2024-06-18 10:10:53 -0400

    Move addToEndpoint to it's own file

--- src/library/operations/ensure-named-graphs.js
+++ src/library/operations/ensure-named-graphs.js
@@ -1,6 +1,4 @@
-import toNT from '@rdfjs/to-ntriples';
-import fetch from 'node-fetch';
-import { Readable } from 'stream';
+import { addToEndpoint } from '../utils/add-to-endpoint.js';
 import { getQuads } from '../utils/fetch-linked-data.js';
 import { parseNamedGraphs } from '../utils/named-graphs.js';
 import { namedGraphs } from './named-graphs.js';
@@ -12,33 +10,4 @@ export async function ensureNamedGraphs(query, endpoint) {
     const quads = await getQuads(graph);
     await addToEndpoint(graph, quads, endpoint);
   }
-}
-
-function toTripleString(quad) {
-  const subject = toNT(quad.subject).replace('_:_:', '_:');
-  const predicate = toNT(quad.predicate).replace('_:_:', '_:');
-  const object = toNT(quad.object).replace('_:_:', '_:');
-  return `${subject} ${predicate} ${object} .\n`;
-}
-
-function* sparqlUpdateIterator(graph, quads) {
-  yield `
-CLEAR GRAPH <${graph}>;
-INSERT DATA {
-GRAPH <${graph}> {
-`;
-  for (const quad of quads) {
-    yield toTripleString(quad);
-  }
-  yield '}}\n';
-}
-
-export async function addToEndpoint(graph, quads, endpoint) {
-  return fetch(endpoint, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/sparql-update',
-    },
-    body: Readable.from(sparqlUpdateIterator(graph, quads)),
-  });
 }
