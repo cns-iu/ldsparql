2971c14380765e67e460163856d9398f267159e7
Bruce W. Herr II <bherr@indiana.edu>
2024-06-18 10:10:37 -0400

    Try to query anyway even if ensure named graph fails

--- src/library/operations/ldsparql.js
+++ src/library/operations/ldsparql.js
@@ -2,6 +2,10 @@ import { sparql } from './sparql.js';
 import { ensureNamedGraphs } from './ensure-named-graphs.js';
 
 export async function ldsparql(query, mimetype, endpoint) {
-  await ensureNamedGraphs(query, endpoint);
+  try {
+    await ensureNamedGraphs(query, endpoint);
+  } catch (err) {
+    console.error(err);
+  }
   return sparql(query, mimetype, endpoint);
 }
