60ca946809d0b4c5cfa3fbdc2803d8acd2844e1a
Bruce W. Herr II <bherr@iu.edu>
2025-09-08 11:34:11 -0400

    Add different routes to test out other backends

--- src/service-worker/ldsparql.js
+++ src/service-worker/ldsparql.js
@@ -1,15 +1,26 @@
-import { OxigraphEndpoint } from '../library/endpoints/';
+import { N3Endpoint, OxigraphEndpoint, QuadstoreEndpoint } from '../library/endpoints/';
 
-function routes(app) {
-  const db = new OxigraphEndpoint();
-  return app.post('/api/ld/sparql', async function (req, res) {
+function endpointRoute(db) {
+  return async function (req, res) {
     const query = (await req.formData()).get('query');
     /** Content Negotiation */
     let mediaType = 'application/sparql-results+json';
 
     const results = await db.query(query, mediaType);
     res.json(results);
-  });
+  };
+}
+
+function routes(app) {
+  const oxigraph = new OxigraphEndpoint();
+  const n3 = new N3Endpoint();
+  const quadstore = new QuadstoreEndpoint();
+
+  return app
+    .post('/api/ld/sparql', endpointRoute(oxigraph))
+    .post('/api/ld/oxigraph/sparql', endpointRoute(oxigraph))
+    .post('/api/ld/n3/sparql', endpointRoute(n3))
+    .post('/api/ld/quadstore/sparql', endpointRoute(quadstore));
 }
 
 export default routes;
